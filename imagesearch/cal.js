8e960c8d54a5d1dd6c34df1bfd7dc5fef51bf952
likhithasl55 <139754073+likhithasl55@users.noreply.github.com>
2023-07-21 15:45:52 +0530

    Add files via upload

---
+++ imagesearch/cal.js
@@ -0,0 +1,49 @@
+const accessKey="Aa6zabudrNDht7jASk4g96RC1k-RQvP9DOPnBgeWQ6Y";
+const formEl = document.querySelector("form");
+const inputEl =document.getElementById("search-input");
+const searchReasults =document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData =""
+let page =1;
+
+async function searchImages(){
+    inputData =inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+    const results =data.results;
+if(page ==1){
+    searchReasults.innerHTML=""
+}
+results.map((result)=>{
+    const imageWrapper =document.createElement('div')
+    imageWrapper.classList.add("search-result")
+    const image =document.createElement('img')
+    image.src =result.urls.small
+    image.addEventListenerlt =result.alt_description
+    const imageLink = document.createElement('a')
+    imageLink.href=result.links.innerHTML
+    imageLink.target ="_blank"
+    imageLink.textContent=result.alt_description
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink)
+    imageWrapper.appendChild(imageWrapper)
+});
+page++
+if(page >1){
+    showMore.style.display ="block";
+}
+}
+
+formEl.addEventListener("submit",(Event)=>{
+    Event.preventDefault()
+    page=1;
+    searchImages()
+})
+
+showMore.addEventListener("click",()=>{
+    searchImages()
+})
